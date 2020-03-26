121911152e1bf60287e6d033c77e652a24fa1617
Emily Yu <bluerainmango@gmail.com>
2020-03-26 03:56:37 -0700

    created build files for production

---
+++ build/precache-manifest.b66188f1bcf88fa4480eb5c643ac88f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c101d71016c43bb95688ed6a25c66ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c75fd87a10804c4f728",
+    "url": "/static/css/main.5102dbb2.chunk.css"
+  },
+  {
+    "revision": "579f28069bb02058c1bc",
+    "url": "/static/js/2.cbfaddd0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cbfaddd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c75fd87a10804c4f728",
+    "url": "/static/js/main.ec558541.chunk.js"
+  },
+  {
+    "revision": "fac60da6912c35275601",
+    "url": "/static/js/runtime-main.c45da950.js"
+  }
+]);
